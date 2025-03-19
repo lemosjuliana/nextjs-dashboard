c5f125c8d45b470a012fae834caac54bbf788974
Juliana Lemos <lemos.julianajesus@gmail.com>
2025-03-19 15:38:43 -0500

    latest changes

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,30 +1,31 @@
-import postgres from 'postgres';
+// import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-async function listInvoices() {
-    const data = await sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
-  `;
+// or
 
-    return data;
-}
+// const sql = postgres(process.env.DATABASE_URL,  { ssl: 'verify-full' });
+
+// async function listInvoices() {
+// 	const data = await sql`
+//     SELECT invoices.amount, customers.name
+
+//     FROM invoices
+//     JOIN customers ON invoices.customer_id = customers.id
+//     WHERE invoices.amount = 666;
+//   `;
+
+// 	return data;
+// }
 
 export async function GET() {
-  try {
-    const data = await listInvoices();
-    return new Response(JSON.stringify(data), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
+  // try {
+  // 	return Response.json(await listInvoices());
+  // } catch (error) {
+  // 	return Response.json({ error }, { status: 500 });
+  // }
 }
